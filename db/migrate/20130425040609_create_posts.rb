e129976e58c76f6237113c90dd37733e427c6e7b
asbubam <asbubam@gmail.com>
2013-04-25 17:42:22 +0900

    modify style

--- db/migrate/20130425040609_create_posts.rb
+++ db/migrate/20130425040609_create_posts.rb
@@ -1,6 +1,9 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
+			t.integer :user_id, :null => false, :default => 0	
+			t.string :title
+			t.text :content, :null => false
 
       t.timestamps
     end
