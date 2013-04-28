f1a31d97549a9c196da0c9579a27e295456b0eb4
asbubam <asbubam@gmail.com>
2013-04-28 15:20:34 +0900

    add post's edit, delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
 	before_filter :login_required
+	before_filter :permission_check, :except => [:index, :new, :create]
 
 	def new
 		@post = Post.new
@@ -13,5 +14,35 @@ class PostsController < ApplicationController
 		end
 
 		redirect_to root_path
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update 
+		@post = Post.find(params[:id])
+		
+		if @post.update_attributes(params[:post])
+			redirect_to  root_path, notice:'Post was successfully updated.'
+		else
+			render :action => "edit"
+		end
+	end
+
+	def destroy
+		unless @post.destroy
+			flash[:notice] = "destroy post failed."
+		end
+		
+		redirect_to root_path
+	end
+
+protected
+	def permission_check
+		@post = Post.find(params[:id])
+		unless @post.user.id == @current_user.id
+			raise PermissionException
+		end
 	end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ActiveRecord::Base
 	attr_accessible :user_id, :title, :content
 
 	belongs_to :user
+
+	def is_owned?(u)
+		self.user.id == u.id
+	end
 end
